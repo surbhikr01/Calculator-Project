439d0fbed5c70bb8cdd66a1d71234921ce19915e
surbhikr01 <shurbhikr01@gmail.com>
2021-06-28 20:42:30 +0530

    commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-function display(val) {
-    document.getElementById("screen").value += val
+let screen = document.getElementById('screen');
+let screenValue = '';
+buttons = document.querySelectorAll('button');
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        //    console.log('Button text is =',buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+            screenValue = "";
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
 }
-function clear() {
-    document.getElementById("screen").value = "";
-}
-function backspace() {
-    let screenval = ducument.getElementById("screen").value;
-    screenval = screenval.substring(0, screenval.length - 1);
-    screen.value = screenval;
-    return screen.value;
-}
-function result() {
-    let x = document.getElementById("screen").value;
-    let y = eval(x);
-    document.getElementById("screen").value = y;
-}
-
