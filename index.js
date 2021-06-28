f8c1a5431fd8ada0dd7ce7e54852e91ab8a9fef7
surbhikr01 <shurbhikr01@gmail.com>
2021-06-28 20:08:17 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,18 @@
+function display(val) {
+    document.getElementById("screen").value += val
+}
+function clear() {
+    document.getElementById("screen").value = "";
+}
+function backspace() {
+    let screenval = ducument.getElementById("screen").value;
+    screenval = screenval.substring(0, screenval.length - 1);
+    screen.value = screenval;
+    return screen.value;
+}
+function result() { }
+let x = document.getElementById("screen").value;
+let y = eval(x);
+document.getElementById("screen").value = y;
+}
+
