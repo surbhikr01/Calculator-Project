8a30400e19a89920cfd4d2fa655e0b7cd97e0159
surbhikr01 <shurbhikr01@gmail.com>
2021-06-30 23:35:07 +0530

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ buttons = document.querySelectorAll('button');
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        //    console.log('Button text is =',buttonText);
+
         if (buttonText == 'X') {
             buttonText = '*';
             screenValue += buttonText; CustomElementRegistry
